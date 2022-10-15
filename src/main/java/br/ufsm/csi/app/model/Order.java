c4bc9329b3d9ebd283317300afaa72c66eaf2050
Deborablara <debora.blara19@gmail.com>
2022-10-15 11:26:36 -0300

    main: add order model

---
+++ src/main/java/br/ufsm/csi/app/model/Order.java
@@ -0,0 +1,78 @@
+package br.ufsm.csi.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Order {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  private Client client;
+
+  @ManyToOne
+  private Vehicle vehicle;
+
+  @ManyToOne
+  private Product product;
+  private Float amount;
+
+  @Enumerated(EnumType.STRING)
+  private Status status = Status.WAITING_FOR_PRODUCTION;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Client getClient() {
+    return client;
+  }
+
+  public void setClient(Client client) {
+    this.client = client;
+  }
+
+  public Vehicle getVehicle() {
+    return vehicle;
+  }
+
+  public void setVehicle(Vehicle vehicle) {
+    this.vehicle = vehicle;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  public Float getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Float amount) {
+    this.amount = amount;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+}
