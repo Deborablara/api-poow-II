baee22ef5f7cae4ef86dab7d32dac63e17cc531c
Deborablara <debora.blara19@gmail.com>
2022-10-27 11:20:32 -0300

    main: change desactive product function

--- src/main/java/br/ufsm/csi/app/controller/ProductController.java
+++ src/main/java/br/ufsm/csi/app/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,12 +49,11 @@ public class ProductController {
     return ResponseEntity.ok().build();
   }
 
-  // @PutMapping("/{id}")
-  // @Transactional
-  // public ResponseEntity<?> desactiveProdcut(@PathVariable Long id) {
-  // productRepository.desactiveProduct(id);
-  // return ResponseEntity.ok().build();
-  // }
+  @PutMapping("/{id}")
+  @Transactional
+  public int desactiveProduct(@PathVariable Long id) {
+    return productRepository.desactiveProduct(id);
+  }
 
   @PutMapping("/update/{id}")
   @Transactional
--- src/main/java/br/ufsm/csi/app/repository/ProductRepository.java
+++ src/main/java/br/ufsm/csi/app/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package br.ufsm.csi.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import br.ufsm.csi.app.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+  @Modifying
+  @Query("UPDATE Product p SET p.active=false WHERE p.id in ?1")
+  int desactiveProduct(Long id);
 }
